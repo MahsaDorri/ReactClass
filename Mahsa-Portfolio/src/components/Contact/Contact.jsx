a079ce60fb08dabbcb6c78f185e8827458e0e666
Mahsa Dorri <mahsadorri093@gmail.com>
2024-03-07 17:20:52 -0500

    Your commit message

---
+++ Mahsa-Portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:mahsadorri093@gmail.com">mahsadorri093@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/mahsa-dorri-5a0981207/">linkedin.com/in/mahsa-dorri-5a0981207/</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/MahsaDorri">github.com/MahsaDorri</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
